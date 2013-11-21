611137584cbd3e9367c6e9feea7109cd3da976b1
halfblood <halfblood369@gmail.com>
2013-11-21 10:10:59 +0800

    Update rsync.js

--- rsync.js
+++ rsync.js
@@ -2,7 +2,7 @@ var Config = require('./lib/preconfig');
 
 var sync = function (host, config) { 
    console.log(host);
-   var cmd = require('util').format('rsync -zrtopg --progress --delete -avz --exclude "%s" -e "ssh -p%s -i %s " %s %s@%s:%s',config.exclude, config.port,config.keyFile,config.src,config.username,host,config.dest);
+   var cmd = require('util').format('rsync -zrtopg --progress --delete -avz --exclude "%s" --exclude "%s" -e "ssh -p%s -i %s " %s %s@%s:%s',config.excludeLog, config.excludePid, config.port,config.keyFile,config.src,config.username,host,config.dest);
    console.error(cmd);
    require('child_process').exec(cmd, function(err,data) {
       if (err) throw err;
